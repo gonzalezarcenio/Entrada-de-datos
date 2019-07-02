ee537821e05ae92f7fcca5bdd7021d948dc4723b
Bryan <guamanbryan1995@hotmail.com>
2019-07-02 22:31:41 +0100

    primera version

---
+++ Aplicativo.java
@@ -0,0 +1,23 @@
+       
+
+	import java.util.Scanner;
+
+
+   	public class Aplicativo {
+   	 public static void main(String[] args) {
+     
+        float numerador,numerador1,denominador,denominador1;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Diguite numerador :"); 
+        numerador= entrada.nextFloat();
+        System.out.println("Diguite numerador :"); 
+        numerador1= entrada.nextFloat();
+        System.out.println("Diguite denominador :"); 
+        denominador= entrada.nextFloat();
+        System.out.println("Diguite denominador :"); 
+        denominador1= entrada.nextFloat();
+        
+        
+    }
+    
+}
